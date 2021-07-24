1c07d30427220e9f32884e1c79547a960d058a90
spooky <84251442+cupidhack@users.noreply.github.com>
2021-07-24 12:48:46 -0700

    quick fork

---
+++ lllc/builtin_functions/utils.py
@@ -0,0 +1,13 @@
+from lllc.ast import parse_to_ast
+from lllc.old_codegen.context import Context
+from lllc.old_codegen.global_context import GlobalContext
+from lllc.old_codegen.stmt import parse_body
+
+
+def generate_inline_function(code, variables, memory_allocator):
+    ast_code = parse_to_ast(code)
+    new_context = Context(
+        vars=variables, global_ctx=GlobalContext(), memory_allocator=memory_allocator
+    )
+    generated_lll = parse_body(ast_code.body, new_context)
+    return new_context, generated_lll
---
+++ main.py
@@ -0,0 +1,2 @@
+from lllc.cli import vyper_lll
+vyper_lll._parse_cli_args()
